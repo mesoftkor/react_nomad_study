a70699342211d2f82cec2704df818fab3d46c42e
SAI_NOTE_AMD <gykim@siwooent.com>
2024-02-01 23:34:03 +0900

    study_2024-02-01

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
+
 function App() {
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event) => setToDo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === "") {
+            return;
+        }
+
+        setToDos((currentArray) => [toDo, ...currentArray]);
+        setToDo("");
+    };
+    console.log(toDos);
     return (
         <div>
-            <h1 className={styles.title}>welcome back!!!!</h1>
-            <Button text={"Continue"} />
+            <h1>My To Dos ({toDos.length})</h1>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do...." />
+                <button>Add To Do</button>
+            </form>
+            <hr />
+            <ul>
+                {toDos.map((item, index) => (
+                    <li key={index}>{item.toUpperCase()}</li>
+                ))}
+            </ul>
         </div>
     );
 }

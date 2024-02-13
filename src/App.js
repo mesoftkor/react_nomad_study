0d8ffbd0e4e061237b83dbce10cbbe6dbf61e112
SAI_NOTE_AMD <gykim@siwooent.com>
2024-02-13 21:44:24 +0900

    gh-pages 최종 커밋

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import Detail from "./routes/Detail";
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route>
-            <Route path={`${process.env.PUBLIC_URL}/`} element={Home} />
-            <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={Detail} />
+            <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+            <Route path={`/movie/:id`} element={<Detail />} />
         </Route>
     )
 );
 
 function App() {
-    console.log(process.env.PUBLIC_URL);
     return <RouterProvider router={router} />;
 }
 
